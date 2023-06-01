da4be45a1fcbd9d7c108f33da238c630608a7e40
Abednego <abedikroos15@gmail.com>
2023-06-01 17:47:10 +0300

    Bears Lab

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES
+    ('Mr. Chocolate', 23, 'M', 'Black', 'Calm', 1),
+    ('Rowdy', 30, 'M', 'Brown', 'Calm', 1),
+    ('Tabitha', 21, 'F', 'White', 'Playful', 1),
+    ('Sergeant Brown', 34, 'M', 'Black', 'Aggressive', 1),
+    ('Melissa', 20, 'F', 'Black', 'Aggressive', 1),
+    ('Grinch', 34, 'M', 'Brown', 'Playful', 1),
+    ('Wendy', 26, 'F', 'White', 'Calm', 1),
+    (NULL, 47, 'F', 'White', 'Aggressive', 1);
+
+
+
